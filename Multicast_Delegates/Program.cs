4e62e6d97fcc733e029ad5d2b6367855f5771117
Alexandre Oliveira <ao.sanches.ao@gmail.com>
2023-09-16 12:50:08 -0300

    Instanciação de sistema que utiliza Multicast delegates

---
+++ Multicast_Delegates/Program.cs
@@ -0,0 +1,24 @@
+﻿using Multicast_Delegates.Services;
+using System;
+
+namespace Delegates
+{
+    class Program
+    {
+        delegate void NumericOperation(double n1, double n2);
+
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o primeiro número: ");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("Digite o segundo número: ");
+            double b = double.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            NumericOperation op = CalculationService.ShowSum;
+            op += CalculationService.ShowMax;
+            
+            op(a, b);
+        }
+    }
+}
